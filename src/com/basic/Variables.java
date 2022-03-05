bb25513958e646faf4da33bcf84e0d40aa18b29a
Muntasir Abdullah Mizan <muntasir.abdullah01@gmail.com>
2022-03-05 20:16:33 +0600

    Java Basic and OOP

---
+++ src/com/basic/Variables.java
@@ -0,0 +1,21 @@
+package com.basic;
+
+public class Variables {
+    int num1=100;
+
+    short num4=100;
+
+    String name="Mr.John";
+
+    char myChar= 'a';
+
+    float number1=10.100000f;
+
+    double number3=10000.10000d;
+
+    boolean myStatus= true;
+    boolean myStatus2= false;
+
+
+}
+
